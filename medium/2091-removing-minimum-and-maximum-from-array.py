a91f29ee7ab683557ab64e58af25fb54477bbfad
M.Adithya Natarajan <adithyamsss2006@gmail.com>
2026-09-02 10:05:01 +0530

    Add solution for LeetCode 2091

---
+++ medium/2091-removing-minimum-and-maximum-from-array.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def minimumDeletions(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        min_idx = nums.index(min(nums))
+        max_idx = nums.index(max(nums))
+
+        left = min(min_idx, max_idx)
+        right = max(min_idx, max_idx)
+
+        remove_front = right + 1
+        remove_back = n - left
+        remove_both_sides = (left + 1) + (n - right)
+
+        return min(remove_front, remove_back, remove_both_sides)
